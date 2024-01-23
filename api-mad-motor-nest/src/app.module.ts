36609aa19672bcda5a44c7a9dff266b9a77c1bc5
Mohaek <elkasmimoha@gmail.com>
2024-01-23 20:28:50 +0100

    Refactoring

--- api-mad-motor-nest/src/app.module.ts
+++ api-mad-motor-nest/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { CategoriasModule } from './categorias/categorias.module'
-import { BbddModule } from './configuracion/bbdd/bbdd.module'
+import { CategoriasModule } from './rest/categorias/categorias.module'
+import { BbddModule } from './config/bbdd/bbdd.module'
 
 @Module({
   imports: [ConfigModule.forRoot(), BbddModule, CategoriasModule],
