6ef539095f46185f74c81a27dbb2278646433714
Mohaek <elkasmimoha@gmail.com>
2024-01-24 12:24:24 +0100

    config

--- api-mad-motor-nest/src/app.module.ts
+++ api-mad-motor-nest/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CategoriasModule } from './rest/categorias/categorias.module'
-import { DatabaseModule } from './config/bbdd/database.module'
+import { DatabaseModule } from './config/database/database.module'
 import { VehiculosModule } from './rest/vehiculos/vehiculos.module'
 
 @Module({
