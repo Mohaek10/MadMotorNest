2ff1d5907b9a6bafd944b8cfb20cb11e3bfb24e1
Mohaek <elkasmimoha@gmail.com>
2024-01-30 18:51:26 +0100

    Added endpoint and services storage

---
+++ api-mad-motor-nest/src/rest/storage/storage.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Logger,
+  Res,
+} from '@nestjs/common'
+import { Response } from 'express'
+import { StorageService } from './storage.service'
+
+@Controller('storage')
+export class StorageController {
+  private readonly logger = new Logger(StorageController.name)
+
+  constructor(private readonly storageService: StorageService) {}
+
+  @Get(':nombreFich')
+  async obtenerFichero(
+    @Param('nombreFich') nombreFich: string,
+    @Res() res: Response,
+  ) {
+    this.logger.log(`Obteniendo fichero ${nombreFich}`)
+    const fichero = this.storageService.encontrarFichero(nombreFich)
+    this.logger.log(`Fichero obtenido ${nombreFich}`)
+    res.sendFile(await fichero)
+  }
+
+  @Get()
+  async obtenerTodosLosFicheros() {
+    this.logger.log(`Obteniendo todos los ficheros con sus urls`)
+    return await this.storageService.obtenerTodasLasImagenesConUrls()
+  }
+}
