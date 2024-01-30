27361afbe17d76fe0eb8c0c90b015a0a23e4a946
miviru <miviru98@hotmail.com>
2024-01-30 20:22:36 +0100

    controller, service y module personal

--- api-mad-motor-nest/src/rest/personal/personal.controller.ts
+++ api-mad-motor-nest/src/rest/personal/personal.controller.ts
@@ -6,40 +6,44 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  ParseIntPipe,
 } from '@nestjs/common'
 import { PersonalService } from './personal.service'
 import { CreatePersonalDto } from './dto/create-personal.dto'
 import { UpdatePersonalDto } from './dto/update-personal.dto'
+import { Paginate, PaginateQuery } from 'nestjs-paginate'
 
 @Controller('personal')
 export class PersonalController {
   constructor(private readonly personalService: PersonalService) {}
 
   @Post()
-  create(@Body() createPersonalDto: CreatePersonalDto) {
-    return this.personalService.create(createPersonalDto)
+  @HttpCode(201)
+  async create(@Body() createPersonalDto: CreatePersonalDto) {
+    return await this.personalService.create(createPersonalDto)
   }
 
   @Get()
-  findAll() {
-    return this.personalService.findAll()
+  async findAll(@Paginate() query: PaginateQuery) {
+    return await this.personalService.findAll(query)
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.personalService.findOne(+id)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.personalService.findOne(id)
   }
 
   @Patch(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updatePersonalDto: UpdatePersonalDto,
   ) {
-    return this.personalService.update(+id, updatePersonalDto)
+    return await this.personalService.update(+id, updatePersonalDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.personalService.remove(+id)
+  async remove(@Param('id') id: string) {
+    return await this.personalService.remove(+id)
   }
 }
