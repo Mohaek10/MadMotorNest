ec6dbef02c48336a6f34678cf95084a0fc326b75
Mohaek <elkasmimoha@gmail.com>
2024-02-06 18:13:51 +0100

    Conflictos

--- api-mad-motor-nest/src/rest/pieza/dto/create-pieza.dto.ts
+++ api-mad-motor-nest/src/rest/pieza/dto/create-pieza.dto.ts
@@ -1,41 +1,31 @@
-import {IsBoolean, IsNotEmpty, IsNumber, IsString, Length, Min} from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Length, Min } from 'class-validator'
 
 export class CreatePiezaDto {
-<<<<<<< Updated upstream
-    @IsNotEmpty()
-    @IsString()
-    @Length(2, 100, {
-        message: 'El nombre debe tener entre 2 y 100 caracteres',
-    })
-    nombre?: string
-    @IsNotEmpty()
-    @IsString()
-    @Length(2, 100, {
-        message: 'El descripcion debe tener entre 2 y 100 caracteres',
-    })
-    descripcion?: string
-    @IsNotEmpty()
-    @IsString()
-    @Length(2, 100, {
-        message: 'La imagen debe ser una url valida',
-    })
-    imagen?: string
-    @IsNotEmpty()
-    @IsNumber()
-    @Min(0)
-    precio?: number
-    @IsNotEmpty()
-    @IsNumber()
-    @Min(0)
-    stock?: number
-    isDeleted?:boolean
-}
-=======
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 100, {
+    message: 'El nombre debe tener entre 2 y 100 caracteres',
+  })
   nombre?: string
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 100, {
+    message: 'El descripcion debe tener entre 2 y 100 caracteres',
+  })
   descripcion?: string
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 100, {
+    message: 'La imagen debe ser una url valida',
+  })
   imagen?: string
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   precio?: number
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   stock?: number
   isDeleted?: boolean
 }
->>>>>>> Stashed changes
