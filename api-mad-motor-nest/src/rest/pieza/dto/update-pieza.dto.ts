518a4b2e2e210f4842921ec71bf736d158499e3d
Rubenoide03 <rubenfernandezp03@gmail.com>
2024-02-06 20:22:39 +0100

    Update Pieza Dto Fixed

--- api-mad-motor-nest/src/rest/pieza/dto/update-pieza.dto.ts
+++ api-mad-motor-nest/src/rest/pieza/dto/update-pieza.dto.ts
@@ -1,4 +1,41 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { CreatePiezaDto } from './create-pieza.dto'
+import {IsNumber, IsOptional, IsString, Length, Min} from "class-validator";
 
-export class UpdatePiezaDto extends PartialType(CreatePiezaDto) {}
+export class UpdatePiezaDto extends PartialType(CreatePiezaDto) {
+    @IsOptional()
+    @IsString()
+    @Length(2, 100, {
+        message: 'El campo marca debe tener entre 2 y 100 caracteres',
+    })
+    nombre?: string;
+    @IsOptional()
+    @IsString()
+    @Length(2, 100, {
+        message: 'El campo descripcion debe tener entre 2 y 100 caracteres',
+    })
+    descripcion?: string;
+    @IsOptional()
+    @IsString()
+    @Length(2, 100, {
+        message: 'La imagen debe ser una url valida',
+    })
+    imagen?: string;
+    @IsOptional()
+    @IsNumber()
+    @Min(0,{
+        message: 'El campo precio debe ser mayor a 0',
+
+    })
+    precio?: number;
+    @IsOptional()
+    @IsNumber()
+    @Min(0,{
+        message: 'El campo stock debe ser mayor a 0',
+    })
+    stock?: number;
+
+
+
+
+}
