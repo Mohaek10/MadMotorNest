3de25abf0bba59c3d5731f0d5b932e40f062bebb
peepp <epepeppe@gmail.com>
2024-02-12 13:01:59 +0100

    Added Docs in Pieza Endpoint and fixed security issues

--- api-mad-motor-nest/src/rest/pieza/dto/update-pieza.dto.ts
+++ api-mad-motor-nest/src/rest/pieza/dto/update-pieza.dto.ts
@@ -1,36 +1,69 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreatePiezaDto } from './create-pieza.dto'
-import { IsNumber, IsOptional, IsString, Length, Min } from 'class-validator'
+import { IsNumber, IsOptional, IsString, Length, Min } from 'class-validator';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { CreatePiezaDto } from './create-pieza.dto';
 
 export class UpdatePiezaDto extends PartialType(CreatePiezaDto) {
+  @ApiProperty({
+    description: 'Nombre de la pieza',
+    minLength: 2,
+    maxLength: 100,
+    default: 'Pieza 1',
+  })
   @IsOptional()
   @IsString()
   @Length(2, 100, {
     message: 'El campo marca debe tener entre 2 y 100 caracteres',
   })
-  nombre?: string
+  nombre?: string;
+
+  @ApiProperty({
+    description: 'Descripción de la pieza',
+    minLength: 2,
+    maxLength: 100,
+    default: 'descripcion 1',
+  })
   @IsOptional()
   @IsString()
   @Length(2, 100, {
     message: 'El campo descripcion debe tener entre 2 y 100 caracteres',
   })
-  descripcion?: string
+  descripcion?: string;
+
+  @ApiProperty({
+    description: 'URL de la imagen de la pieza',
+    minLength: 2,
+    maxLength: 100,
+    default: 'https://via.placeholder.com/150',
+  })
   @IsOptional()
   @IsString()
   @Length(2, 100, {
-    message: 'La imagen debe ser una url valida',
+    message: 'La imagen debe ser una URL válida',
   })
-  imagen?: string
+  imagen?: string;
+
+  @ApiProperty({
+    description: 'Precio de la pieza',
+    minimum: 0,
+    default: 3,
+  })
   @IsOptional()
   @IsNumber()
   @Min(0, {
     message: 'El campo precio debe ser mayor a 0',
   })
-  precio?: number
+  precio?: number;
+
+  @ApiProperty({
+    description: 'Cantidad de la pieza',
+    minimum: 0,
+    default:4,
+  })
   @IsOptional()
   @IsNumber()
   @Min(0, {
     message: 'El campo cantidad debe ser mayor a 0',
+
   })
-  cantidad?: number
+  cantidad?: number;
 }
